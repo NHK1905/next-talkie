65101ff1846a25f1fca58218c53a84bb4c9a306a
NHK1905 <nguyenhuykhang195@gmail.com>
2023-08-11 02:53:14 +0700

    Update users, conversations screen, conversations box and create messages

--- app/hooks/useConversation.ts
+++ app/hooks/useConversation.ts
@@ -2,19 +2,22 @@ import { useParams } from "next/navigation";
 import { useMemo } from "react";
 
 const useConversation = () => {
-    const params = useParams()
+    const params = useParams();
 
     const conversationId = useMemo(() => {
         if (!params?.conversationId) {
-            return ''
+            return '';
         }
-    }, [params?.conversationId])
-    const isOpen = useMemo(() => !!conversationId, [conversationId])
+
+        return params.conversationId as string;
+    }, [params?.conversationId]);
+
+    const isOpen = useMemo(() => !!conversationId, [conversationId]);
 
     return useMemo(() => ({
         isOpen,
-        conversationId,
-    }), [isOpen, conversationId])
-}
+        conversationId
+    }), [isOpen, conversationId]);
+};
 
-export default useConversation
+export default useConversation;
